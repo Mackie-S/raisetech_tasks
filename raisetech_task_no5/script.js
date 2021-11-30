beee1fa171eb1f51aec5fb681860309da1ea3698
Mackie <92088651+Mackie-S@users.noreply.github.com>
2021-11-30 22:23:59 +0900

    Add files via upload

---
+++ raisetech_task_no5/script.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  $(".has-sub").hover(function(){
+    $(this).children(".sub").stop().slideToggle('fast');
+    $(this).children(".plus").toggleClass(".active");
+  });
+
+  let $window = $(this).width();
+  let bp = 1200;
+
+  if($window > bp){
+    $(".has-sub").show();
+  }
+
+  $(".menuBtn").on("click", function(){
+    $(".has-sub").slideToggle("fast");
+    $(".menuBtn").toggleClass("active")
+  });
+
+  $(window).resize(function(){
+    let $window = $(this).width();
+    let bp = 1200;
+    
+    if($window > bp){
+      $(".has-sub").show();
+      $(".menuBtn").removeClass("active");
+    }else{
+      $(".has-sub").hide();
+      $(".menuBtn").removeClass("active");
+    }
+  });
+
+});
